b3eb2e47abdaacf2b039731cb3b9b9fc755f28c8
Raidel <raidelmfernandez@gmail.com>
2025-10-28 22:30:16 +0100

    commit final

--- App.tsx
+++ App.tsx
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import { Alert, StyleSheet, Text, View } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import Marcador from './components/Marcador'
@@ -149,4 +128,4 @@ const styles = StyleSheet.create({
   }
 })
 
->>>>>>> origin/integracion
+
